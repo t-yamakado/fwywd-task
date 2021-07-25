e139b9598b037c3652516e6e1e858e1b07819925
yamakado <worldtravel811@gmail.com>
2021-07-25 17:14:04 +0900

    add: body tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,91 +5,96 @@ import { informationList, valueList } from "../definitions/commonDefinitions";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen px-2 flex flex-col justify-center items-center">
-			<Head>
-				<title>Takana Mayo App</title>
-				<meta name="description" content="Welcome to Takana Mayo app" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+		<body>
+			<div className="min-h-screen px-2 flex flex-col justify-center items-center">
+				<Head>
+					<title>Takana Mayo App</title>
+					<meta
+						name="description"
+						content="Welcome to Takana Mayo app"
+					/>
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
 
-			<main className="py-20 flex-1 flex flex-col justify-center items-center">
-				<h1 className="text-center text-6xl">
-					Welcome to{" "}
-					<span className="text-green-500">Takana Mayo Page</span>
-				</h1>
+				<main className="py-20 flex-1 flex flex-col justify-center items-center">
+					<h1 className="text-center text-6xl">
+						Welcome to{" "}
+						<span className="text-green-500">Takana Mayo Page</span>
+					</h1>
 
-				<p className="text-center leading-normal text-2xl pt-6">
-					私について <span className="text-blue-400">ABOUT</span>
-				</p>
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-4 gap-4">
-					{informationList.map((info) => {
-						return (
-							<InformationCard
-								imageUrl={info.imageUrl}
-								title={info.title}
-								content={info.content}
-								key={info.type}
-							/>
-						);
-					})}
-				</div>
-				<p className="text-center leading-normal text-2xl pt-6">
-					スキル <span className="text-blue-400">SKILLS</span>
-				</p>
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 p-4 gap-4 w-full">
-					<div
-						style={{
-							backgroundImage: `url(./skill.png)`,
-							backgroundRepeat: "no-repeat",
-							backgroundSize: "contain",
-						}}
-					></div>
-					<SkillSet />
-				</div>
-				<p className="text-center leading-normal text-2xl pt-6">
-					価値観 <span className="text-blue-400">VALUES</span>
-				</p>
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-4 gap-4">
-					{valueList.map((info) => {
-						return (
-							<InformationCard
-								imageUrl={info.imageUrl}
-								title={info.title}
-								content={info.content}
-								key={info.type}
-							/>
-						);
-					})}
-				</div>
-				<p className="text-center leading-normal text-2xl pt-6">
-					３年後にやりたいこと{" "}
-					<span className="text-blue-400">FUTURE</span>
-				</p>
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 p-4 gap-4">
-					<div>
-						<p>ここにテキスト</p>
+					<p className="text-center leading-normal text-2xl pt-6">
+						私について <span className="text-blue-400">ABOUT</span>
+					</p>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-4 gap-4">
+						{informationList.map((info) => {
+							return (
+								<InformationCard
+									imageUrl={info.imageUrl}
+									title={info.title}
+									content={info.content}
+									key={info.type}
+								/>
+							);
+						})}
 					</div>
-					<div
-						style={{
-							backgroundImage: `url(./future.png)`,
-							backgroundRepeat: "no-repeat",
-							backgroundSize: "contain",
-						}}
-					></div>
-				</div>
-			</main>
+					<p className="text-center leading-normal text-2xl pt-6">
+						スキル <span className="text-blue-400">SKILLS</span>
+					</p>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 p-4 gap-4 w-full">
+						<div
+							style={{
+								backgroundImage: `url(./skill.png)`,
+								backgroundRepeat: "no-repeat",
+								backgroundSize: "contain",
+							}}
+						></div>
+						<SkillSet />
+					</div>
+					<p className="text-center leading-normal text-2xl pt-6">
+						価値観 <span className="text-blue-400">VALUES</span>
+					</p>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-4 gap-4">
+						{valueList.map((info) => {
+							return (
+								<InformationCard
+									imageUrl={info.imageUrl}
+									title={info.title}
+									content={info.content}
+									key={info.type}
+								/>
+							);
+						})}
+					</div>
+					<p className="text-center leading-normal text-2xl pt-6">
+						３年後にやりたいこと{" "}
+						<span className="text-blue-400">FUTURE</span>
+					</p>
+					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 p-4 gap-4">
+						<div>
+							<p>ここにテキスト</p>
+						</div>
+						<div
+							style={{
+								backgroundImage: `url(./future.png)`,
+								backgroundRepeat: "no-repeat",
+								backgroundSize: "contain",
+							}}
+						></div>
+					</div>
+				</main>
 
-			<footer className="w-screen border-b-2 border-green-500">
-				<div className="mx-auto px-6">
-					<div className="border-t-2 border-gray-300 flex flex-col items-center">
-						<div className="sm:w-2/3 text-center py-6">
-							<p className="text-sm text-green-500 font-bold mb-2">
-								© 2021 by Takana Mayo
-							</p>
+				<footer className="w-screen border-b-2 border-green-500">
+					<div className="mx-auto px-6">
+						<div className="border-t-2 border-gray-300 flex flex-col items-center">
+							<div className="sm:w-2/3 text-center py-6">
+								<p className="text-sm text-green-500 font-bold mb-2">
+									© 2021 by Takana Mayo
+								</p>
+							</div>
 						</div>
 					</div>
-				</div>
-			</footer>
-		</div>
+				</footer>
+			</div>
+		</body>
 	);
 }
