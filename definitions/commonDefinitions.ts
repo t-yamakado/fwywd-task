42f2c4dc6edbe59e42aff06708429ed0000b271c
yamakado <worldtravel811@gmail.com>
2021-08-01 21:08:57 +0900

    modify: 固定文言を長さがあるものに変更

--- definitions/commonDefinitions.ts
+++ definitions/commonDefinitions.ts
@@ -3,19 +3,22 @@ export const informationList = [
 		type: "Interest",
 		imageUrl: "about-1.png",
 		title: "趣味",
-		content: "ここに趣味のことについて書きたいね。",
+		content:
+			"ここにテキストここにテキストここにテキストここにテキストここにテキストここにテキスト",
 	},
 	{
 		type: "Food",
 		imageUrl: "about-2.png",
 		title: "好きな食べ物",
-		content: "ここに食べ物のことについて書きたいね。",
+		content:
+			"ここにテキストここにテキストここにテキストここにテキストここにテキストここにテキスト",
 	},
 	{
 		type: "Personality",
 		imageUrl: "about-3.png",
 		title: "性格",
-		content: "ここに性格のことについて書きたいね。",
+		content:
+			"ここにテキストここにテキストここにテキストここにテキストここにテキストここにテキスト",
 	},
 ];
 
@@ -24,18 +27,21 @@ export const valueList = [
 		type: "Value01",
 		imageUrl: "value1.png",
 		title: "価値観その1",
-		content: "ここに価値観のことについて書きたいね。",
+		content:
+			"ここにテキストここにテキストここにテキストここにテキストここにテキストここにテキスト",
 	},
 	{
 		type: "Value02",
 		imageUrl: "value2.png",
 		title: "価値観その2",
-		content: "ここに価値観のことについて書きたいね。",
+		content:
+			"ここにテキストここにテキストここにテキストここにテキストここにテキストここにテキスト",
 	},
 	{
 		type: "Value03",
 		imageUrl: "value3.png",
 		title: "価値観その3",
-		content: "ここに価値観のことについて書きたいね。",
+		content:
+			"ここにテキストここにテキストここにテキストここにテキストここにテキストここにテキスト",
 	},
 ];
